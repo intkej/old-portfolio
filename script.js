4890cce9ce823ab96f6d3da4aa78b60218a3208f
Johannes <124790333+notnipa@users.noreply.github.com>
2023-12-29 16:13:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const textElement = document.getElementById("text");
+const cursorElement = document.getElementById("cursor");
+
+let charIndex = 0;
+
+function typeMainText() {
+    const currentText = 'intke.net';
+    if (charIndex < currentText.length) {
+        textElement.textContent += currentText[charIndex];
+        charIndex++;
+        setTimeout(typeMainText, 300);
+    } 
+}
+
+setTimeout(() => {
+    typeMainText();
+}, 3000);
+
+function changePage(page) {
+    var pageURL;
+    switch (page) {
+      case 'services':
+        pageURL = 'services.html';
+        break;
+      case 'aboutme':
+        pageURL = 'moreaboutme.html';
+        break;
+      default:
+        pageURL = 'index.html';
+    }
+
+    window.location.href = pageURL;
+}
